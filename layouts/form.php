0568e4dc93af137234bdc128c220d5692564d8e4
Alex <work@geqo.ru>
2018-09-27 18:16:59 +0300

    Ready for JED

--- layouts/form.php
+++ layouts/form.php
@@ -1,5 +1,6 @@
 <?php
 /*
+    Licensed under the GNU AGPL v3
     This file is part of GComments module (mod_gcomments)
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
